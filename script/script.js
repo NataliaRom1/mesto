322e8a7f85f0d12c54cd3c45bd05fde77d602cb1
Nata <Natalia.Romanovskaia@skoltech.ru>
2022-12-29 19:35:24 +0300

    style: изменены названия папок, классов и путь к изображению

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-const popupElement = document.querySelector('.pop-up');
-const popupCloseButtonElement = popupElement.querySelector('.pop-up__btn-close');
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__btn-close');
 const popupOpenButtonElement = document.querySelector('.profile__btn-edit');
 
 const openPopup = function () {
-  popupElement.classList.add('pop-up__opened');
+  popupElement.classList.add('popup__opened');
 }
 const closePopup = function () {
-  popupElement.classList.remove('pop-up__opened');
+  popupElement.classList.remove('popup__opened');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
 
-let formElement = popupElement.querySelector('.pop-up__btn-save');
-let nameInput = popupElement.querySelector('.pop-up__name');
-let jobInput = popupElement.querySelector('.pop-up__description');
+let formElement = popupElement.querySelector('.popup__btn-save');
+let nameInput = popupElement.querySelector('.popup__name');
+let jobInput = popupElement.querySelector('.popup__description');
 let profileNameElement = document.querySelector('.profile__name');
 let profileDescriptionElement = document.querySelector('.profile__description');
 
